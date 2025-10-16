5e14471cd6b71b0bf6069310a68b0a6bd97b297e
Shankhalika <88712498+ShankhalikaMallick@users.noreply.github.com>
2025-10-16 12:08:33 +0530

    Add output report for associative array functions

---
+++ Day6_associative_array_functions.sv
@@ -0,0 +1,25 @@
+// output in EDAplayground
+
+Contains Synopsys proprietary information.
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Oct 16 02:35 2025
+number of elements in a1=6
+value at index 0 is 0
+value at index 4 is 40
+value at index 12 is 120
+value at index 16 is 160
+value at index 20 is 200
+index 12 exists in a1? 1
+last index is 20
+first index is 0
+next index after first is 4
+previous index before is 0
+value at index blue is 4
+value at index green is 3
+value at index red is 1
+value at index 100 is 20
+value at index 101 is 15
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 0 ns
+CPU Time:      0.470 seconds;       Data structure size:   0.0Mb
+Thu Oct 16 02:35:43 2025
+Done
